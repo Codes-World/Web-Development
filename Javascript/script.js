89a40f9b16fbbdf11946b6134eccb40888344972
Codes-World <aman.soni6200@gmail.com>
2025-06-15 16:12:44 +0530

    Javascrip code

---
+++ Javascript/script.js
@@ -0,0 +1,20 @@
+// var a = 6;
+// var b = 7;
+// b = 8; //We can change value of b variable
+// var c = a + b;
+// alert(c);
+// alert("Hello Aman!");
+
+// const v = 7;
+// v = 8; //we can not change value of v variable because this variable is constant
+// alert(v);
+// document.write("Hello Mini!");
+// console.log("Hello GF!");
+
+function result(){
+    alert("Hello there!");
+}
+
+function colorMe(){
+    document.body.style.backgroundColor = "red";
+}
