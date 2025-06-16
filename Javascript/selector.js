521b54821a51465cb3e12bae9c4c294c66fe74d4
Codes-World <aman.soni6200@gmail.com>
2025-06-16 13:14:41 +0530

    Selector

---
+++ Javascript/selector.js
@@ -0,0 +1,7 @@
+function design(){
+    // document.getElementById("brand").style.color = "red";
+    // document.getElementById("brand").style.fontSize = "20px";
+    var h1 = document.getElementById("brand");
+    h1.style.color = "red";
+    h1.style.fontSize = "100px"
+}
