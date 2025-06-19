877addce963b09ff89d8547f287d6a87e7752d51
Codes-World <aman.soni6200@gmail.com>
2025-06-19 14:02:42 +0530

    Event

---
+++ Javascript/event.js
@@ -0,0 +1,10 @@
+// Parameter (Parameterised variable)
+function changeColor(colorName){
+    var h1 = document.getElementById("title");
+    h1.style.color = colorName;
+}
+
+function bodyStyle(){
+    var body = document.body;
+    body.style.backgroundColor = "red";
+}
