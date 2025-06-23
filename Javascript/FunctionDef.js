04ca93eb6f6b18a32dba6b30a0b5c226ff857de7
Codes-World <aman.soni6200@gmail.com>
2025-06-23 13:57:46 +0530

    Function

---
+++ Javascript/FunctionDef.js
@@ -0,0 +1,17 @@
+// Optional paramter bgColor = "cyan"
+// function size(idValue, bgColor="cyan"){
+//     var button = document.getElementById(idValue);
+//     button.style.width = "120px";
+//     button.style.height = "40px";
+//     button.style.backgroundColor = bgColor;
+//     button.style.color = "white";
+//     button.style.borderWidth = 0;
+// }
+
+function size(button){
+    button.style.width = "120px";
+    button.style.height = "40px";
+    button.style.backgroundColor = "red";
+    button.style.color = "white";
+    button.style.borderWidth = 0;
+}
